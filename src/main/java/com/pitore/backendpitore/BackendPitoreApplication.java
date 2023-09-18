2ed91f7bcb15327aa0ac4034fc21ae268a2a4a55
Madness2002 <73316834+Madness2002@users.noreply.github.com>
2023-09-18 11:28:54 -0500

    a

--- src/main/java/com/pitore/backendpitore/BackendPitoreApplication.java
+++ src/main/java/com/pitore/backendpitore/BackendPitoreApplication.java
@@ -18,7 +18,7 @@ public class BackendPitoreApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("backend-pitore-production.up.railway.app").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
