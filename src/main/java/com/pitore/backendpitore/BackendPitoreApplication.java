baaad4811b6123a5a352e26b8c690ed5ff53d8ac
Madness__ <fernandomori2002@gmail.com>
2023-11-14 20:30:38 -0500

    ds

--- src/main/java/com/pitore/backendpitore/BackendPitoreApplication.java
+++ src/main/java/com/pitore/backendpitore/BackendPitoreApplication.java
@@ -9,8 +9,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
+
 @SpringBootApplication
 public class BackendPitoreApplication {
 
